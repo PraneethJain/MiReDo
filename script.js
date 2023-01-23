795817fecfefbc9c904e3ff4cb12e2005fb113b3
PraneethJain <praneethjain005@gmail.com>
2023-01-23 20:13:15 +0530

    Added game logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let LEVEL = 3;
+let delay = 400;
+
 // Utility Functions
 
 const turnObjToArray = function (obj) {
@@ -36,11 +39,30 @@ function fade_out(element) {
   }, 50);
 }
 
+const disable_notes = () => {
+  notes_arr.forEach((note) => {
+    note.style["pointer-events"] = "none";
+  });
+};
+
+const enable_notes = () => {
+  notes_arr.forEach((note) => {
+    note.style["pointer-events"] = "auto";
+  });
+};
+
+const black_all = () => {
+  notes_arr.forEach((note) => {
+    note.classList.remove("filled");
+  });
+};
+
 let sounds = [];
 for (let i = 1; i <= 8; ++i) {
   sounds.push(new Audio(`./sounds/${i}.mp3`));
 }
-
+let generating_level = false;
+let counter = LEVEL;
 function click_note(i) {
   let x = -1;
   switch (i) {
@@ -71,37 +93,58 @@ function click_note(i) {
   }
 
   sounds[x].play();
+
+  if (!generating_level) {
+    counter -= 1;
+    if (i != correct_order[counter]) {
+      // Reset here ok
+      console.log("Get fucked");
+    }
+
+    if (counter == 0) {
+      disable_notes();
+      setTimeout(nextLevel, 1500);
+    }
+  } else {
+    correct_order.push(i);
+  }
 }
 
-let delay = 400;
+const nextLevel = () => {
+  correct_order = [];
+  counter = LEVEL;
+  black_all();
+  level(LEVEL);
+};
 
 let start_page = document.querySelector(".start");
 let container = document.querySelector(".container");
 start_page.addEventListener("click", (e) => {
+  disable_notes();
   fade_out(start_page);
-  setTimeout(level, delay * 4);
+  setTimeout(level.bind(null, LEVEL), delay * 4);
 });
 
 let notes = document.querySelectorAll(".container div div");
 let notes_arr = turnObjToArray(notes);
+let clicked = [];
+let correct_order = [];
 
 for (let i = 0; i < 8; ++i) {
   let note = notes[i];
 
   note.addEventListener("click", (e) => {
     note.classList.add("filled");
+    clicked.push(i);
     click_note(i);
   });
 }
 
-let n = 3;
-
-const stage = () => {
-  n -= 1;
-  if (n >= 0) {
-    let note = selected_notes[n];
+const setup = (n) => {
+  if (n > 0) {
+    let note = selected_notes[n - 1];
     note.click();
-    setTimeout(stage, delay);
+    setTimeout(setup.bind(null, n - 1), delay);
   }
 };
 
@@ -109,10 +152,15 @@ const end = () => {
   selected_notes.forEach((note) => {
     note.classList.remove("filled");
   });
+  enable_notes();
+  clicked = [];
+  generating_level = false;
 };
 
-const level = () => {
-  window.selected_notes = shuffle(notes_arr).slice(0, n);
-  stage();
-  setTimeout(end, delay * (n + 2));
+const level = (level_num) => {
+  window.selected_notes = shuffle(notes_arr).slice(0, level_num);
+  generating_level = true;
+  setup(level_num);
+
+  setTimeout(end, delay * (level_num + 2));
 };
