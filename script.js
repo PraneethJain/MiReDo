b1af38a1a49fd587a9cbdb1ec9f6d64be629bee9
PraneethJain <praneethjain005@gmail.com>
2023-01-23 15:06:35 +0530

    Added sounds in cyclic order

--- script.js
+++ script.js
@@ -26,12 +26,46 @@ function shuffle(array) {
 let notes = document.querySelectorAll(".container div div");
 let notes_arr = turnObjToArray(notes);
 
+console.log(notes);
+
 for (let i = 0; i < 8; ++i) {
   let note = notes[i];
-  note.addEventListener("click", (e) => {});
+  let x = -1;
+  switch (i) {
+    case 0:
+      x = 1;
+      break;
+    case 1:
+      x = 8;
+      break;
+    case 2:
+      x = 7;
+      break;
+    case 3:
+      x = 2;
+      break;
+    case 4:
+      x = 6;
+      break;
+    case 5:
+      x = 3;
+      break;
+    case 6:
+      x = 4;
+      break;
+    case 7:
+      x = 5;
+      break;
+  }
+
+  let cur_sound = new Audio(`./sounds/${x}.mp3`);
+
+  note.addEventListener("click", (e) => {
+    cur_sound.play();
+  });
 }
 
-let n = 5;
+let n = 3;
 const selected_notes = shuffle(notes_arr).slice(0, n);
 
 selected_notes.forEach((note) => {
